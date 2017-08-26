4493408decf148deca193d43e344f89c4c28ce07
Rayhaneh Banyassady <r.b.assady@gmail.com>
2017-08-26 19:11:08 +0000

    Simple Ruby exercises.

---
+++ exe03.rb
@@ -0,0 +1,8 @@
+puts "What is 3 + 2? #{3 + 2}"
+puts "Is #{5} greater than #{-2}? #{5 > -2}"
+
+puts "I will now count my chickens:"
+
+puts "Hens #{25 + 30 / 6}"
+puts "Roosters #{100 - 25 * 3 % 4}"
+
---
+++ exe18.rb
@@ -0,0 +1,7 @@
+# count = 10
+def print_one (str)
+  # puts count
+  puts "You wanted to print #{str}"
+end
+
+print_one('test')
